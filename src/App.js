4605e103f3a394a6e78107e74445386b21edf2b8
huyvda <vuducanhhuy@gmail.com>
2024-05-11 21:04:16 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello my first app</h1>
+      <h2>vu duc anh huy</h2>
+    </div>
+  );
+}
+
+export default App;
